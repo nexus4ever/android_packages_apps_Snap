c0c49052d47997245633a0b91c1830dfe4ff4129
zfu <zfu@codeaurora.org>
2016-03-07 15:14:53 +0800

    Fix android.process.media got killed when capture
    android.process.media has a very low priority as Cached app in background.
    When snapdragon camera using ContentResolver and do query/updating it will
    promot to foreground priority, but there may have some race condition
    that lmk in the progress to kill media, and AMS want set to foreground.
    So, keep this process as foreground during the whole ActivityLife time,
    by hold a query cursor onCreate and destory onStop.
    
    CRs-Fixed: 984061
    Change-Id: I530fddfa240030ad84dc09b599ad783f6a2f21e0
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -249,6 +249,7 @@ public class CameraActivity extends Activity
     // FilmStripView.setDataAdapter fires 2 onDataLoaded calls before any data is actually loaded
     // Keep track of data request here to avoid creating useless UpdateThumbnailTask.
     private boolean mDataRequested;
+    private Cursor mCursor;
 
     private WakeLock mWakeLock;
 
@@ -1435,6 +1436,8 @@ public class CameraActivity extends Activity
         } else {
             mSecureCamera = intent.getBooleanExtra(SECURE_CAMERA_EXTRA, false);
         }
+
+        mCursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
 
         if (mSecureCamera) {
             // Change the window flags so that secure camera can show when locked
