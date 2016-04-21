0e3aa239f313906a3d591737c82abbd1822fbf75
Steve Kondik <shade@chemlab.org>
2016-04-20 17:02:20 -0700

    Snap: Add support for focus distance
    * Hook up available metadata to the new focus manager.
    
    Change-Id: I793df67a2682d8bb2dee994193111227dd8c8d5a

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -150,6 +150,7 @@ public class PhotoModule
     private static final int SWITCH_TO_GCAM_MODULE = 12;
     private static final int ON_PREVIEW_STARTED = 13;
     private static final int UPDATE_GESTURES_UI = 14;
+    private static final int SET_FOCUS_RATIO = 15;
 
     // The subset of parameters we need to update in setCameraParameters().
     private static final int UPDATE_PARAM_INITIALIZE = 1;
@@ -503,6 +504,11 @@ public class PhotoModule
 
                 case UPDATE_GESTURES_UI: {
                     updateGesturesUI();
+                    break;
+                }
+
+                case SET_FOCUS_RATIO: {
+                    mUI.getFocusRing().setRadiusRatio((Float)msg.obj);
                     break;
                 }
             }
@@ -958,6 +964,14 @@ public class PhotoModule
             mCameraDevice.stopFaceDetection();
             mUI.onStopFaceDetection();
         }
+    }
+
+    @Override
+    public void setFocusRatio(float ratio) {
+        mHandler.removeMessages(SET_FOCUS_RATIO);
+        Message m = mHandler.obtainMessage(SET_FOCUS_RATIO);
+        m.obj = ratio;
+        mHandler.sendMessage(m);
     }
 
     // TODO: need to check cached background apps memory and longshot ION memory
@@ -1487,6 +1501,8 @@ public class PhotoModule
                     setCameraState(IDLE);
                     break;
             }
+            mCameraDevice.refreshParameters();
+            mFocusManager.setParameters(mCameraDevice.getParameters());
             mFocusManager.onAutoFocus(focused, mUI.isShutterPressed());
         }
     }
@@ -1497,6 +1513,8 @@ public class PhotoModule
         @Override
         public void onAutoFocusMoving(
                 boolean moving, CameraProxy camera) {
+            mCameraDevice.refreshParameters();
+            mFocusManager.setParameters(mCameraDevice.getParameters());
             mFocusManager.onAutoFocusMoving(moving);
         }
     }
@@ -3828,6 +3846,7 @@ public class PhotoModule
             }
 
             mCameraDevice.setParameters(mParameters);
+            mFocusManager.setParameters(mParameters);
 
             // Switch to gcam module if HDR+ was selected
             if (doModeSwitch && !mIsImageCaptureIntent) {
