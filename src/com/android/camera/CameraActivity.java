fae263db55232865bc8fdb1bcdf3f56ee730ad24
Jay Wang <jaywang@codeaurora.org>
2016-06-24 15:58:31 -0700

    SnapdragonCamera: Fix first-time crash and cleanup permissions
    As onCreate skips the creation of setting manager when the critical
    permissions are not granted, this causes the onDestroy to call
    setting manager destroy function with null pointer.
    Fix the issue with null check.
    In addition, remove unused permissions from manifest file
    
    Change-Id: I99b5f09449aacfb7eb9a5771e446df75956a9827
    CRs-Fixed: 1034202

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -1663,7 +1663,7 @@ public class CameraActivity extends Activity
                         PackageManager.PERMISSION_GRANTED &&
                 checkSelfPermission(Manifest.permission.RECORD_AUDIO) ==
                         PackageManager.PERMISSION_GRANTED &&
-                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
                         PackageManager.PERMISSION_GRANTED) {
             mHasCriticalPermissions = true;
         } else {
