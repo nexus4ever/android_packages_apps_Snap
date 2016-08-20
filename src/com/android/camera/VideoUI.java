bfd484757b7723b96c4226bc7620f46c1656e603
spezi77 <spezi77@gmx.com>
2016-08-20 11:24:48 +0200

    VideoUI: never show video rec. pause button. * Currently not working
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/android/camera/VideoUI.java
+++ src/com/android/camera/VideoUI.java
@@ -876,6 +876,7 @@ public class VideoUI implements PieRenderer.PieListener,
     public void showRecordingUI(boolean recording) {
         mRecordingStarted = recording;
         mMenuButton.setVisibility(recording ? View.GONE : View.VISIBLE);
+        mPauseButton.setVisibility(View.GONE);
         if (recording) {
             mShutterButton.setImageResource(R.drawable.shutter_button_video_stop);
             hideSwitcher();
